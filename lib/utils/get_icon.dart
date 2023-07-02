7851e2ca1f0ea6cec42e4757d41868d2ae19f041
Fredianto <fredinix@proton.me>
2023-07-02 15:00:59 +0700

    feat: get icon

---
+++ lib/utils/get_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
+
+Icon icond(IconData icon) => Icon(icon, color: Colors.blue);
+
+Icon getIcon(int condition) {
+  switch (condition) {
+    case < 300:
+      return icond(WeatherIcons.thunderstorm);
+    case > 300 && < 400:
+      return icond(WeatherIcons.rain_mix);
+    case > 400 && < 600:
+      return icond(WeatherIcons.rain);
+    case > 600 && < 700:
+      return icond(WeatherIcons.snowflake_cold);
+    case > 700 && < 800:
+      return icond(WeatherIcons.day_windy);
+    case == 800:
+      return icond(WeatherIcons.day_sunny);
+    case > 800 && <= 804:
+      return icond(WeatherIcons.cloud);
+    default:
+      return const Icon(Icons.abc, color: Colors.transparent);
+  }
+}
