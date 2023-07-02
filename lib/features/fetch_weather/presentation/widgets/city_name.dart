b406d154abed87504efe5d5b88feb57eb5e164ab
Fredianto <fredinix@proton.me>
2023-07-02 17:16:54 +0700

    feat: city name widget

---
+++ lib/features/fetch_weather/presentation/widgets/city_name.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CityName extends StatelessWidget {
+  const CityName({super.key, required this.city});
+  final String city;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+      child: Text(
+        city,
+        textAlign: TextAlign.left,
+        style: const TextStyle(fontSize: 35),
+      ),
+    );
+  }
+}
