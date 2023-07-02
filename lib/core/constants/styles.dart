5dc5b3b09d71dd9d507426f7acb308678c9c0e46
Fredianto <fredinix@proton.me>
2023-07-02 17:45:06 +0700

    feat: style const

---
+++ lib/core/constants/styles.dart
@@ -0,0 +1,13 @@
+part of 'constants.dart';
+
+const textField = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city, color: Colors.white, size: 25),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(color: Colors.black),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(13)),
+    borderSide: BorderSide.none,
+  ),
+);
