9950868dd023d8642040586989dd7947b7f14913
Fredianto <fredinix@proton.me>
2023-07-02 14:55:30 +0700

    feat: get text

---
+++ lib/utils/get_text.dart
@@ -0,0 +1,12 @@
+String getText(int temp) {
+  switch (temp) {
+    case < 17:
+      return 'You\'ll need 🧣 and 🧤';
+    case > 28 && < 33:
+      return 'Time for shorts, shades,  and 👕';
+    case > 33:
+      return 'It\'s 🍦 time';
+    default:
+      return 'Bring a 🧥 & ☂ (just in case)';
+  }
+}
