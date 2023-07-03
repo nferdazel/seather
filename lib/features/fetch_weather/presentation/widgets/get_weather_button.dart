69ee3b1f96e6dec2f038dcd9e64815a9ccf5581d
Fredianto <fredinix@proton.me>
2023-07-03 08:54:18 +0700

    refactor: get weather button

--- lib/features/fetch_weather/presentation/widgets/get_weather_button.dart
+++ lib/features/fetch_weather/presentation/widgets/get_weather_button.dart
@@ -2,13 +2,13 @@ part of 'weather_widgets.dart';
 
 /// TODO: implement Cubit here to get city value
 class GetWeatherButton extends StatelessWidget {
-  const GetWeatherButton(this.city, {super.key});
-  final String? city;
+  const GetWeatherButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () => Navigator.pop(context, city),
+      // onPressed: () => Navigator.pop(context, city),
+      onPressed: () {},
       child: const Center(
         child: Text(
           'Get Weather',
